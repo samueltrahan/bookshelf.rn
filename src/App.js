46387f2e0cb57e1defac44abea114a64911e7524
Samuel Trahan <samtrahan91@gmail.com>
2020-09-30 13:58:45 -0500

    search bar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 
 const App = () => {
+  const [books, setBooks] = useState([])
+
+
   return (
     <>
-    Hello world
+    
     </>
   )
 }
