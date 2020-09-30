c199c6500bdc46284b134dadd5926232045aebcb
Samuel Trahan <samtrahan91@gmail.com>
2020-09-30 15:32:51 -0500

    on input change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './SearchBar.css'
 
 const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState('')
+
+  const onInputChange = (event) => {
+    event.preventDefault();
+    setSearchTerm(event.target.value)
+  }
+
+  
   return (
     <>
     <div className="ui form">
       <form className="field">
     <label>Search Books</label>
-    <input id="input" className="input" type="text"></input>
+    <input id="input" value={searchTerm} onChange={onInputChange} className="input" type="text"></input>
       </form>
     </div>
     </>
