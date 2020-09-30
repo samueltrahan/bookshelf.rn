57c916b52ab7ebf2e01bc72e2181e98cf157ce81
Samuel Trahan <samtrahan91@gmail.com>
2020-09-30 14:08:30 -0500

    nav bar styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import './NavBar.css'
 
 const NavBar = () => {
   return (
     <>
-    NavBar
+    <nav className="nav-bar">
+      <ul className="right">
+      <li><a href='/'>Search Books</a></li>
+      </ul>
+    </nav>
     </>
   )
 }
